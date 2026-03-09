da30e1ca7f4c1ad0187e9364eb13c8694101a1c1
JamesN05 <jamespaulnagle5@gmail.com>
2026-03-09 22:25:16 +0000

    Created a function within SaveFace that takes in a camera image with co ordinates and saves it to a bitmap which will then create a smaller bitmap from that one

--- Facial Recognition Glasses App/SaveFace.java
+++ Facial Recognition Glasses App/SaveFace.java
@@ -1,2 +1,20 @@
-Bitmap bitmap = imageToBitmap(cameraImage);
+public class SaveFace {
+
+    public static void cropAndSaveFace(cameraImage, imageX, imageY) {
+
+        //Save image to a bitmap so we can edit and crop it before storing to database
+        Bitmap savedPicture = imageToBitmap(cameraImage);
+
+        //Save a smaller picture to a bitmap
+        Bitmap facialScan = Bitmap.createBitmap(
+                savedPicture,
+                faceX = imageX,
+                faceY = imageY,
+                200,
+                200
+        );
+
+        //Save to file
+    }
+}
 
