d02795e6a473e163f77d738e7f2fb6763f8532de
JamesN05 <jamespaulnagle5@gmail.com>
2026-03-11 03:13:21 +0000

    Created the FaceScan Class. This class contains a method which will be called from Sam's class after the green box appears around the person's face. This method takes a photo of the person and saves it to a bitmap (with 200 * 200 px measurements) and then saves it to a JPEG file in a scannedFaces folder on the persons phone. In the future once we have made the database, we will add a way for sending the picture along with the person's details to the firebase database. (Will add comments explaining code in depth tomorrow)

--- app/src/main/java/com/example/facialrecognitionapp/FaceScan.java
+++ app/src/main/java/com/example/facialrecognitionapp/FaceScan.java
@@ -1,25 +1,63 @@
 package com.example.facialrecognitionapp;
 
 import android.graphics.Bitmap;
+import android.content.Context;
+import android.graphics.Rect;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FaceScan {
 
-    static int faceX;
-    static int faceY;
+    public static void cropAndSaveFace(Context context,
+                                       Bitmap cameraFrame,
+                                       Rect faceBounds) throws IOException {
 
-    public static void cropAndSaveFace(Bitmap cameraImage, int imageX, int imageY) {
+        int faceX = Math.max(0, faceBounds.left);
+        int faceY = Math.max(0, faceBounds.top);
 
         //Save image to a bitmap so we can save it to a file onto android phone before
         //transferring it to the database
         Bitmap facialScan = Bitmap.createBitmap(
-                cameraImage,
-                faceX = imageX,
-                faceY = imageY,
+                cameraFrame,
+                faceX,
+                faceY,
                 200,
                 200
         );
 
-        //Save to file
+        File path = new File(context.getFilesDir(),
+                             "ScannedFaces");
+        if (!path.exists()) {
 
+            boolean directoryCreated = path.mkdirs();
+
+            if (!directoryCreated) {
+
+                throw new IOException("Failed to create directory: " + path.getAbsolutePath());
+            }
+        }
+
+        //Save to file
+        File faceFile;
+        int faceNumber = 1;
+
+        do {
+
+            faceFile = new File(path, "myface_" + Integer.toString(faceNumber) + ".jpg");
+            faceNumber++;
+        }
+        while (faceFile.exists());
+
+        try (FileOutputStream out = new FileOutputStream(faceFile)) {
+
+            facialScan.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
     }
 }
