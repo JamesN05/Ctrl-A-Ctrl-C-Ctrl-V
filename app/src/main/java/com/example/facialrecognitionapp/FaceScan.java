c789ea0dd6c69ea656493420273f4d6ebd8ca5bb
JamesN05 <jamespaulnagle5@gmail.com>
2026-03-11 01:31:05 +0000

    Added project to master attempt 2

--- app/src/main/java/com/example/facialrecognitionapp/FaceScan.java
+++ app/src/main/java/com/example/facialrecognitionapp/FaceScan.java
@@ -1,4 +1,24 @@
 package com.example.facialrecognitionapp;
 
+import android.graphics.Bitmap;
+
 public class FaceScan {
+
+    static int faceX;
+    static int faceY;
+
+    public static void cropAndSaveFace(Bitmap cameraImage, int imageX, int imageY) {
+
+        //Save image to a bitmap so we can save it to a file onto android phone before
+        //transferring it to the database
+        Bitmap facialScan = Bitmap.createBitmap(
+                cameraImage,
+                faceX = imageX,
+                faceY = imageY,
+                200,
+                200
+        );
+
+        //Save to file
+    }
 }
