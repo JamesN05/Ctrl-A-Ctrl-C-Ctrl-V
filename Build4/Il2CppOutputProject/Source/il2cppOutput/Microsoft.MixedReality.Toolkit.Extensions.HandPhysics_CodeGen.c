09d995271d768683ecc7f36b85fb0949e5bc93c1
James Nagle <jamespaulnagle5@gmail.com>
2026-04-14 01:55:48 +0100

    Face detection story, api detects face, sends info about faceand co ordinates back to unity, saves a picture of face and green box debug showing the detection of the face

---
+++ Build4/Il2CppOutputProject/Source/il2cppOutput/Microsoft.MixedReality.Toolkit.Extensions.HandPhysics_CodeGen.c
@@ -0,0 +1,248 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::.ctor(System.String,System.UInt32,Microsoft.MixedReality.Toolkit.BaseMixedRealityProfile)
+extern void HandPhysicsService__ctor_m52429795C4CEF39BD65589393D5656059BC5C7B3 (void);
+// 0x00000002 Microsoft.MixedReality.Toolkit.Input.IMixedRealityHandJointService Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::get_HandJointService()
+extern void HandPhysicsService_get_HandJointService_m5019083DED943B4503378B598902E97CCCD430B4 (void);
+// 0x00000003 UnityEngine.GameObject Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::get_HandPhysicsServiceRoot()
+extern void HandPhysicsService_get_HandPhysicsServiceRoot_mF0A6F6A05B598D92713CDDCDD5665D51F7170314 (void);
+// 0x00000004 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::set_HandPhysicsServiceRoot(UnityEngine.GameObject)
+extern void HandPhysicsService_set_HandPhysicsServiceRoot_m88FCA70B3F880FB49A9C86B1E739993345B7D2A7 (void);
+// 0x00000005 System.Int32 Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::get_HandPhysicsLayer()
+extern void HandPhysicsService_get_HandPhysicsLayer_m5E110EF766D5C3398FB152E15AAC9AB080325FB3 (void);
+// 0x00000006 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::set_HandPhysicsLayer(System.Int32)
+extern void HandPhysicsService_set_HandPhysicsLayer_m1233F7000976C2C33A768D9BD0B0ED376A0EFC72 (void);
+// 0x00000007 System.Boolean Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::get_UsePalmKinematicBody()
+extern void HandPhysicsService_get_UsePalmKinematicBody_m00EDD94AE82B89E0E159E50B6276AA354F3010F5 (void);
+// 0x00000008 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::set_UsePalmKinematicBody(System.Boolean)
+extern void HandPhysicsService_set_UsePalmKinematicBody_m4115F7824EDDE734F4A68F6D5CE01EEB28E0CF4F (void);
+// 0x00000009 UnityEngine.GameObject Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::get_FingerTipKinematicBodyPrefab()
+extern void HandPhysicsService_get_FingerTipKinematicBodyPrefab_m64F5496F631E3A8B3BF94EB97DA1BC62D9D3AB47 (void);
+// 0x0000000A System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::set_FingerTipKinematicBodyPrefab(UnityEngine.GameObject)
+extern void HandPhysicsService_set_FingerTipKinematicBodyPrefab_mA9ABF7E6A1AAE4660DFE0EC2838FFAD65EBF5D7C (void);
+// 0x0000000B UnityEngine.GameObject Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::get_PalmKinematicBodyPrefab()
+extern void HandPhysicsService_get_PalmKinematicBodyPrefab_mFE68A2C2E67658558DBEB8251D91E312AB6F63F5 (void);
+// 0x0000000C System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::set_PalmKinematicBodyPrefab(UnityEngine.GameObject)
+extern void HandPhysicsService_set_PalmKinematicBodyPrefab_m7DE6E37A31F1D744D7B8437F9EF6B0D828372950 (void);
+// 0x0000000D System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::Initialize()
+extern void HandPhysicsService_Initialize_m6FE4E43BB9FEFA7B1856DA5F11FD1F327048947D (void);
+// 0x0000000E System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::Enable()
+extern void HandPhysicsService_Enable_m47EC398AB9C20713504D9B63D9BDC5E436CD92FE (void);
+// 0x0000000F System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::Disable()
+extern void HandPhysicsService_Disable_m6012B85C7AE5D583266FAB3D94B247E4026358D1 (void);
+// 0x00000010 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::Update()
+extern void HandPhysicsService_Update_mD37D4D3C976854DFF7B08E28419E18429E53872B (void);
+// 0x00000011 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::CreateKinematicBodies()
+extern void HandPhysicsService_CreateKinematicBodies_m1BA46F66DD39F6E794EFC4BF76241F308400A3E6 (void);
+// 0x00000012 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::DestroyKinematicBodies()
+extern void HandPhysicsService_DestroyKinematicBodies_m6359346697BCABCCEA62669F0E92E738E1EB8052 (void);
+// 0x00000013 System.Boolean Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::TryCreateJointKinematicBody(UnityEngine.GameObject,System.Int32,Microsoft.MixedReality.Toolkit.Utilities.Handedness,Microsoft.MixedReality.Toolkit.Utilities.TrackedHandJoint,UnityEngine.Transform,Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody&)
+extern void HandPhysicsService_TryCreateJointKinematicBody_m1CC7C5E5A2839948A214B769C88D158D071FCD6E (void);
+// 0x00000014 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsService::.cctor()
+extern void HandPhysicsService__cctor_m01D5CF0BB4B35D98CCBAB5CD15A53A49476BC047 (void);
+// 0x00000015 System.Boolean Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsServiceProfile::get_UsePalmKinematicBody()
+extern void HandPhysicsServiceProfile_get_UsePalmKinematicBody_m1B21CB1D74911E07766D3D8FF3951FACA1C1A215 (void);
+// 0x00000016 UnityEngine.GameObject Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsServiceProfile::get_FingerTipKinematicBodyPrefab()
+extern void HandPhysicsServiceProfile_get_FingerTipKinematicBodyPrefab_mAEBF4216C5D1AA8810955CFEAD582C7C97967236 (void);
+// 0x00000017 UnityEngine.GameObject Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsServiceProfile::get_PalmKinematicBodyPrefab()
+extern void HandPhysicsServiceProfile_get_PalmKinematicBodyPrefab_mCEF65AB67220CEEABD6A3855642A1EB94CB076D0 (void);
+// 0x00000018 System.Int32 Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsServiceProfile::get_HandPhysicsLayer()
+extern void HandPhysicsServiceProfile_get_HandPhysicsLayer_m3A0D883747DD49AD5D0CE9AE6341E735068AFFE5 (void);
+// 0x00000019 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.HandPhysicsServiceProfile::.ctor()
+extern void HandPhysicsServiceProfile__ctor_mF436233297B76D7CACE3017331B01B68B16A91A9 (void);
+// 0x0000001A UnityEngine.GameObject Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.IHandPhysicsService::get_HandPhysicsServiceRoot()
+// 0x0000001B System.Int32 Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.IHandPhysicsService::get_HandPhysicsLayer()
+// 0x0000001C System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.IHandPhysicsService::set_HandPhysicsLayer(System.Int32)
+// 0x0000001D System.Boolean Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.IHandPhysicsService::get_UsePalmKinematicBody()
+// 0x0000001E System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.IHandPhysicsService::set_UsePalmKinematicBody(System.Boolean)
+// 0x0000001F UnityEngine.GameObject Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.IHandPhysicsService::get_FingerTipKinematicBodyPrefab()
+// 0x00000020 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.IHandPhysicsService::set_FingerTipKinematicBodyPrefab(UnityEngine.GameObject)
+// 0x00000021 UnityEngine.GameObject Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.IHandPhysicsService::get_PalmKinematicBodyPrefab()
+// 0x00000022 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.IHandPhysicsService::set_PalmKinematicBodyPrefab(UnityEngine.GameObject)
+// 0x00000023 UnityEngine.Transform Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::get_Joint()
+extern void JointKinematicBody_get_Joint_m948E3B5BA24B178BBA6D2F4D29F98752557515F7 (void);
+// 0x00000024 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::set_Joint(UnityEngine.Transform)
+extern void JointKinematicBody_set_Joint_mF976E4E19DE584EB5675778DC90711242A786ED5 (void);
+// 0x00000025 Microsoft.MixedReality.Toolkit.Utilities.Handedness Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::get_HandednessType()
+extern void JointKinematicBody_get_HandednessType_m923572414F42D4A1A6BFC431D445DC6F598E18CA (void);
+// 0x00000026 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::set_HandednessType(Microsoft.MixedReality.Toolkit.Utilities.Handedness)
+extern void JointKinematicBody_set_HandednessType_m2E422C118D3F30163DA06D3D6D28483EEF1DCA03 (void);
+// 0x00000027 Microsoft.MixedReality.Toolkit.Utilities.TrackedHandJoint Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::get_JointType()
+extern void JointKinematicBody_get_JointType_mA98BD55171719298E04DE2F5569A0739642BE898 (void);
+// 0x00000028 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::set_JointType(Microsoft.MixedReality.Toolkit.Utilities.TrackedHandJoint)
+extern void JointKinematicBody_set_JointType_m4939256A8A6BA9CD654CD4A325654AF701C53BE1 (void);
+// 0x00000029 System.Action`1<Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody> Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::get_OnEnableAction()
+extern void JointKinematicBody_get_OnEnableAction_m176286519EB7592BC806CB8655010AB57412C113 (void);
+// 0x0000002A System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::set_OnEnableAction(System.Action`1<Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody>)
+extern void JointKinematicBody_set_OnEnableAction_m9D5742B7C6308F0B303EC5F0D233011635534C6F (void);
+// 0x0000002B System.Action`1<Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody> Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::get_OnDisableAction()
+extern void JointKinematicBody_get_OnDisableAction_mC6F262913E8E9B57A5B26D5A06F7AA71D932B45F (void);
+// 0x0000002C System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::set_OnDisableAction(System.Action`1<Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody>)
+extern void JointKinematicBody_set_OnDisableAction_mB86D115B00B6CD1A5DEBA01FF988D6E74B0EE6C0 (void);
+// 0x0000002D System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::UpdateState(System.Boolean)
+extern void JointKinematicBody_UpdateState_m11D87E9D8CF51BC0368E437134526161FDF26454 (void);
+// 0x0000002E System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::.ctor()
+extern void JointKinematicBody__ctor_m15ACC74D447C937D9F6B38B5A984DBA2CE0D1D0C (void);
+// 0x0000002F System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.JointKinematicBody::.cctor()
+extern void JointKinematicBody__cctor_mC0B031A31E110CC93CC67183AA29182F5261EFD6 (void);
+// 0x00000030 TMPro.TextMeshPro Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.Examples.PhysicsTriggerEventReadout::get_TextField()
+extern void PhysicsTriggerEventReadout_get_TextField_m99AFFEB8E2B0ED216600BD78537248A29539F90E (void);
+// 0x00000031 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.Examples.PhysicsTriggerEventReadout::set_TextField(TMPro.TextMeshPro)
+extern void PhysicsTriggerEventReadout_set_TextField_mCB5191359CAFB8B011A5E6320E05BF1D14196115 (void);
+// 0x00000032 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.Examples.PhysicsTriggerEventReadout::OnTriggerEnter(UnityEngine.Collider)
+extern void PhysicsTriggerEventReadout_OnTriggerEnter_m25609204F1A8717B2D094C3548810C93E5BD1CD3 (void);
+// 0x00000033 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.Examples.PhysicsTriggerEventReadout::OnTriggerExit(UnityEngine.Collider)
+extern void PhysicsTriggerEventReadout_OnTriggerExit_mA5EAE418DCF67C933B34D33DEB7DA4CF95F514FC (void);
+// 0x00000034 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.Examples.PhysicsTriggerEventReadout::WriteText(System.Boolean)
+extern void PhysicsTriggerEventReadout_WriteText_mF5498C8875E9B431B5E39B0B926BCB863955BC7C (void);
+// 0x00000035 System.Void Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.Examples.PhysicsTriggerEventReadout::.ctor()
+extern void PhysicsTriggerEventReadout__ctor_m9A3D9A9AB2AC5A57160A98DDCF875BB081CA5F96 (void);
+static Il2CppMethodPointer s_methodPointers[53] = 
+{
+	HandPhysicsService__ctor_m52429795C4CEF39BD65589393D5656059BC5C7B3,
+	HandPhysicsService_get_HandJointService_m5019083DED943B4503378B598902E97CCCD430B4,
+	HandPhysicsService_get_HandPhysicsServiceRoot_mF0A6F6A05B598D92713CDDCDD5665D51F7170314,
+	HandPhysicsService_set_HandPhysicsServiceRoot_m88FCA70B3F880FB49A9C86B1E739993345B7D2A7,
+	HandPhysicsService_get_HandPhysicsLayer_m5E110EF766D5C3398FB152E15AAC9AB080325FB3,
+	HandPhysicsService_set_HandPhysicsLayer_m1233F7000976C2C33A768D9BD0B0ED376A0EFC72,
+	HandPhysicsService_get_UsePalmKinematicBody_m00EDD94AE82B89E0E159E50B6276AA354F3010F5,
+	HandPhysicsService_set_UsePalmKinematicBody_m4115F7824EDDE734F4A68F6D5CE01EEB28E0CF4F,
+	HandPhysicsService_get_FingerTipKinematicBodyPrefab_m64F5496F631E3A8B3BF94EB97DA1BC62D9D3AB47,
+	HandPhysicsService_set_FingerTipKinematicBodyPrefab_mA9ABF7E6A1AAE4660DFE0EC2838FFAD65EBF5D7C,
+	HandPhysicsService_get_PalmKinematicBodyPrefab_mFE68A2C2E67658558DBEB8251D91E312AB6F63F5,
+	HandPhysicsService_set_PalmKinematicBodyPrefab_m7DE6E37A31F1D744D7B8437F9EF6B0D828372950,
+	HandPhysicsService_Initialize_m6FE4E43BB9FEFA7B1856DA5F11FD1F327048947D,
+	HandPhysicsService_Enable_m47EC398AB9C20713504D9B63D9BDC5E436CD92FE,
+	HandPhysicsService_Disable_m6012B85C7AE5D583266FAB3D94B247E4026358D1,
+	HandPhysicsService_Update_mD37D4D3C976854DFF7B08E28419E18429E53872B,
+	HandPhysicsService_CreateKinematicBodies_m1BA46F66DD39F6E794EFC4BF76241F308400A3E6,
+	HandPhysicsService_DestroyKinematicBodies_m6359346697BCABCCEA62669F0E92E738E1EB8052,
+	HandPhysicsService_TryCreateJointKinematicBody_m1CC7C5E5A2839948A214B769C88D158D071FCD6E,
+	HandPhysicsService__cctor_m01D5CF0BB4B35D98CCBAB5CD15A53A49476BC047,
+	HandPhysicsServiceProfile_get_UsePalmKinematicBody_m1B21CB1D74911E07766D3D8FF3951FACA1C1A215,
+	HandPhysicsServiceProfile_get_FingerTipKinematicBodyPrefab_mAEBF4216C5D1AA8810955CFEAD582C7C97967236,
+	HandPhysicsServiceProfile_get_PalmKinematicBodyPrefab_mCEF65AB67220CEEABD6A3855642A1EB94CB076D0,
+	HandPhysicsServiceProfile_get_HandPhysicsLayer_m3A0D883747DD49AD5D0CE9AE6341E735068AFFE5,
+	HandPhysicsServiceProfile__ctor_mF436233297B76D7CACE3017331B01B68B16A91A9,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	JointKinematicBody_get_Joint_m948E3B5BA24B178BBA6D2F4D29F98752557515F7,
+	JointKinematicBody_set_Joint_mF976E4E19DE584EB5675778DC90711242A786ED5,
+	JointKinematicBody_get_HandednessType_m923572414F42D4A1A6BFC431D445DC6F598E18CA,
+	JointKinematicBody_set_HandednessType_m2E422C118D3F30163DA06D3D6D28483EEF1DCA03,
+	JointKinematicBody_get_JointType_mA98BD55171719298E04DE2F5569A0739642BE898,
+	JointKinematicBody_set_JointType_m4939256A8A6BA9CD654CD4A325654AF701C53BE1,
+	JointKinematicBody_get_OnEnableAction_m176286519EB7592BC806CB8655010AB57412C113,
+	JointKinematicBody_set_OnEnableAction_m9D5742B7C6308F0B303EC5F0D233011635534C6F,
+	JointKinematicBody_get_OnDisableAction_mC6F262913E8E9B57A5B26D5A06F7AA71D932B45F,
+	JointKinematicBody_set_OnDisableAction_mB86D115B00B6CD1A5DEBA01FF988D6E74B0EE6C0,
+	JointKinematicBody_UpdateState_m11D87E9D8CF51BC0368E437134526161FDF26454,
+	JointKinematicBody__ctor_m15ACC74D447C937D9F6B38B5A984DBA2CE0D1D0C,
+	JointKinematicBody__cctor_mC0B031A31E110CC93CC67183AA29182F5261EFD6,
+	PhysicsTriggerEventReadout_get_TextField_m99AFFEB8E2B0ED216600BD78537248A29539F90E,
+	PhysicsTriggerEventReadout_set_TextField_mCB5191359CAFB8B011A5E6320E05BF1D14196115,
+	PhysicsTriggerEventReadout_OnTriggerEnter_m25609204F1A8717B2D094C3548810C93E5BD1CD3,
+	PhysicsTriggerEventReadout_OnTriggerExit_mA5EAE418DCF67C933B34D33DEB7DA4CF95F514FC,
+	PhysicsTriggerEventReadout_WriteText_mF5498C8875E9B431B5E39B0B926BCB863955BC7C,
+	PhysicsTriggerEventReadout__ctor_m9A3D9A9AB2AC5A57160A98DDCF875BB081CA5F96,
+};
+static const int32_t s_InvokerIndices[53] = 
+{
+	1992,
+	14,
+	14,
+	26,
+	10,
+	32,
+	95,
+	31,
+	14,
+	26,
+	14,
+	26,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	2747,
+	3,
+	95,
+	14,
+	14,
+	10,
+	23,
+	14,
+	10,
+	32,
+	95,
+	31,
+	14,
+	26,
+	14,
+	26,
+	14,
+	26,
+	95,
+	31,
+	10,
+	32,
+	14,
+	26,
+	14,
+	26,
+	31,
+	23,
+	3,
+	14,
+	26,
+	26,
+	26,
+	31,
+	23,
+};
+extern const Il2CppCodeGenModule g_Microsoft_MixedReality_Toolkit_Extensions_HandPhysicsCodeGenModule;
+const Il2CppCodeGenModule g_Microsoft_MixedReality_Toolkit_Extensions_HandPhysicsCodeGenModule = 
+{
+	"Microsoft.MixedReality.Toolkit.Extensions.HandPhysics.dll",
+	53,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
---
+++ Build4/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
@@ -0,0 +1,6 @@
+#include "il2cpp-config.h"
+#include "C:\Code\Unity Projects\Ctrl-A-Ctrl-C-Ctrl-V\Build4\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\BlobReader.cpp"
+#include "C:\Code\Unity Projects\Ctrl-A-Ctrl-C-Ctrl-V\Build4\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\Debugger.cpp"
+#include "C:\Code\Unity Projects\Ctrl-A-Ctrl-C-Ctrl-V\Build4\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
+#include "C:\Code\Unity Projects\Ctrl-A-Ctrl-C-Ctrl-V\Build4\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeSymbol.cpp"
+#include "C:\Code\Unity Projects\Ctrl-A-Ctrl-C-Ctrl-V\Build4\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\VmStringUtils.cpp"
